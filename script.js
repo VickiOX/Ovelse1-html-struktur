c6fa17eca0297f04d74b90b905c7f67fa4d349af
VickiOx <viox0001@stud.kea.dk>
2023-08-24 10:54:19 +0200

    2B done

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const title= document.querySelector("h1");
 console.log("title");
 
-let text= document.querySelector("p");
+let text= document.querySelector("article:nth-child(1) p");
 console.log("text");
+
+title.textContent = "en anden title";
+text.textContent = "halløj";
+
+let text2 = document.querySelector("article:nth-child(2) p")
+text2.textContent = "halløj igen";
+
+
+
+let textp1= document.querySelector("article:nth-child(1) h2");
+textp1.textContent = " halli halløj";
+
+let textp2= document.querySelector("article:nth-child(2) h2");
+textp2.textContent = " halli halløj og et lige øjeblik";
